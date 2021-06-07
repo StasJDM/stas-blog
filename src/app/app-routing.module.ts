10f8c5fe726de914b786d2b5c551393e07133e54
StasJDM <pyatnicyn.stanislav@yandex.ru>
2021-06-07 22:58:09 +0300

    Fixed layout. Added unstaged changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageBlogComponent } from './pages/page-blog/page-blog.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageProfileComponent } from './pages/page-profile/page-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PageHomeComponent },
+  { path: 'blog', component: PageBlogComponent },
+  { path: 'profile', component: PageProfileComponent },
+  { path: '**', component: PageHomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppLayoutComponent } from './components/app-layout/app-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PageBlogComponent } from './pages/page-blog/page-blog.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageProfileComponent } from './pages/page-profile/page-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppLayoutComponent
+    AppLayoutComponent,
+    HeaderComponent,
+    PageBlogComponent,
+    PageHomeComponent,
+    PageProfileComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
