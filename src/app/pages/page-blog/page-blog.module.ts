572334661bb3e316f8d1e71b51538892ef00dbec
StasJDM <pyatnicyn.stanislav@yandex.ru>
2021-06-15 23:07:53 +0300

    Created publication layout

--- src/app/pages/page-blog/page-blog.module.ts
+++ src/app/pages/page-blog/page-blog.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PageBlogRoutingModule } from './page-blog-routing.module';
 import { PageBlogComponent } from './page-blog.component';
-
+import { FeedComponent } from 'src/app/components/feed/feed.component';
+import { PublicationComponent } from 'src/app/components/publication/publication.component';
 
 @NgModule({
-  declarations: [
-    PageBlogComponent
-  ],
-  imports: [
-    CommonModule,
-    PageBlogRoutingModule
-  ]
+  declarations: [PageBlogComponent, FeedComponent, PublicationComponent],
+  imports: [CommonModule, PageBlogRoutingModule],
 })
-export class PageBlogModule { }
+export class PageBlogModule {}
