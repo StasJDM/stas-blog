3673a0cb28d676636336423690fcc60ed386c049
StasJDM <pyatnicyn.stanislav@yandex.ru>
2021-07-10 20:25:30 +0300

    Fixed httpClient tests

--- src/app/components/feed/feed.component.spec.ts
+++ src/app/components/feed/feed.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FeedComponent } from './feed.component';
@@ -8,9 +9,9 @@ describe('FeedComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FeedComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule],
+      declarations: [FeedComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/services/publications-data-service.service.spec.ts
+++ src/app/services/publications-data-service.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { PublicationsDataServiceService } from './publications-data-service.service';
@@ -6,7 +7,7 @@ describe('PublicationsDataServiceService', () => {
   let service: PublicationsDataServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
     service = TestBed.inject(PublicationsDataServiceService);
   });
 
